028169c72e76258ad12afbc75dec6b05e21fb24d
oskarnordin <oskarnordin1@gmail.com>
2025-06-01 17:46:23 +0200

    Updated w mongoose
    mongoose update

--- server.js
+++ server.js
@@ -1,10 +1,69 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/books';
+mongoose.connect(mongoUrl);
+mongoose.Promise = Promise;
+
+const Author = mongoose.model('Author', {
+  name: String,
+});
+
+const Book = mongoose.model('Book', {
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author',
+  },
+});
+
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    const tolkien = new Author({ name: 'J.R.R Tolkien' });
+    await tolkien.save();
+
+    const rowling = new Author({ name: 'J.K. Rowling' });
+    await rowling.save();
+
+    await new Book({
+      title: "Harry Potter and the Philosopher's Stone",
+      author: rowling,
+    }).save();
+    await new Book({
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: rowling,
+    }).save();
+    await new Book({
+      title: 'Harry Potter and the Prisoner of Azkaban',
+      author: rowling,
+    }).save();
+    await new Book({
+      title: 'Harry Potter and the Goblet of Fire',
+      author: rowling,
+    }).save();
+    await new Book({
+      title: 'Harry Potter and the Order of the Phoenix',
+      author: rowling,
+    }).save();
+    await new Book({
+      title: 'Harry Potter and the Half-Blood Prince',
+      author: rowling,
+    }).save();
+    await new Book({
+      title: 'Harry Potter and the Deathly Hallows',
+      author: rowling,
+    }).save();
+    await new Book({ title: 'The Lord of the Rings', author: tolkien }).save();
+    await new Book({ title: 'The Hobbit', author: tolkien }).save();
+  };
+  seedDatabase;
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 27017;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -12,8 +71,30 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', (req, res) => {
+  res.send('Hello Technigo!');
+});
+
+app.get('/authors', async (req, res) => {
+  const author = await Author.find();
+  res.json(authors);
+});
+
+app.get('/books', async (req, res) => {
+  const books = await Book.find().populate('author');
+  res.json(books);
+});
+
+app.get('/authors/:id/books', async (req, res) => {
+  const author = await Author.findById(req.params.id);
+  if (author) {
+    const books = await Book.find({
+      author: mongoose.Types.ObjectId.createFromHexString(author.id),
+    });
+    res.json(books);
+  } else {
+    res.status(404).json({ error: 'Author not found' });
+  }
 });
 
 // Start the server
